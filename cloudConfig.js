33c40952f27d561d6668e0ed1e57434d54872a1e
Shreya <shreyachavan127@gmail.com>
2026-04-03 13:35:13 +0530

    Add Project Files

---
+++ cloudConfig.js
@@ -0,0 +1,29 @@
+const cloudniary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudniary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudniary,
+  params: {
+    folder: "wanderlust_DEV",
+    allowed_formats: ["jpeg", "png", "jpg"],
+  },
+});
+
+module.exports = { 
+  cloudniary, 
+  storage 
+};
+
+
+
+
+
+
+
+
